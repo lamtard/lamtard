53b68dca204a8a04f3411967a762aa498b5e388c
Joe Huss <detain@interserver.net>
2021-12-23 19:18:24 -0500

    updates

--- README.md
+++ README.md
@@ -8,6 +8,14 @@ the project aims to process mail as fast as possible, with a pluggable architect
 
 * https://github.com/walkor/workerman
 
+## RFCs and Specs
+
+* https://tools.ietf.org/html/rfc1939 pop3
+* https://tools.ietf.org/html/rfc5321 smtp
+* https://tools.ietf.org/html/rfc3501 imap
+* https://tools.ietf.org/html/rfc822 mail content analysis
+
+
 ## Related Projects for reference
 
 * [https://github.com/zhusaidong/PHPMailServer](https://github.com/zhusaidong/PHPMailServer)
---
+++ src/Protocols/Smtp.php
@@ -0,0 +1,54 @@
+<?php
+namespace Protocols;
+
+use \Workerman\Protocols\ProtocolInterface;
+use \Workerman\Connection\ConnectionInterface;
+
+class Smtp implements ProtocolInterface
+{
+    /**
+     * Check the integrity of the package
+     * If the packet length can be obtained, return the length of the packet in the buffer, otherwise return 0 to continue waiting for the data
+     * If there is a problem with the protocol, you can return false, and the current client connection will be disconnected
+     * @param string $buffer
+     * @param ConnectionInterface $connection
+     * @return int
+     */
+    public static function input($buffer, ConnectionInterface $connection)
+    {
+        // Get the position of the newline character "\n"
+        $pos = strpos($buffer, "\n");
+        // No line break, no way to know the packet length, return 0 to continue waiting for data
+        if($pos === false)
+        {
+            return 0;
+        }
+        // There is a newline character, returns the current packet length (including the newline character)
+        return $pos+1;
+    }
+
+    /**
+     * Package, it will be called automatically when sending data to the client
+     * @param string $buffer
+     * @param ConnectionInterface $connection
+     * @return string
+     */
+    public static function encode($buffer, ConnectionInterface $connection)
+    {
+        // json serialization, and add a newline character as a mark of the end of the request
+        return json_encode($buffer)."\n";
+    }
+
+    /**
+     * Unpacking, when the number of data bytes received is equal to the value returned by input (value greater than 0), it will be called automatically
+     * And pass to the $data parameter of the onMessage callback function
+     * @param string $buffer
+     * @param ConnectionInterface $connection
+     * @return string
+     */
+    public static function decode($buffer, ConnectionInterface $connection)
+    {
+        // Remove the line breaks and restore to an array
+        return json_decode(trim($buffer), true);
+    }
+}
